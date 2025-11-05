a8afb2aa2297e60f51bcf6a2468d583fa3584b74
http.dluc <http.dluc@gmail.com>
2025-11-05 13:43:37 -0300

    05/11

---
+++ port-iii/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ResponsiveNav from "@/components/Home/NavBar/ResponsiveNav";
+import "./globals.css";
+
+const font = Inter({
+  variable: '--font-Poppins',
+  weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets:['latin'],
+})
+
+export const metadata: Metadata = {
+  title: "Portfolio.III | Next.js 16",
+  description: "portfolio desenvolvido com Next.js 16",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${font.className} antialiased bg-[#0d0d1f]`}>
+          <ResponsiveNav/>
+        {children}
+      </body>
+    </html>
+  );
+}
